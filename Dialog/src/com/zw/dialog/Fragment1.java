948c296cbf17de79b27cc46a795940a6bfa5eb7b
jy02374138 <jy02374138@sina.com>
2016-09-18 11:54:22 +0800

    update

---
+++ Dialog/src/com/zw/dialog/Fragment1.java
@@ -0,0 +1,46 @@
+package com.zw.dialog;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Fragment1 extends DialogFragment{
+	
+	static Fragment1 newInstance(String $title){
+		Fragment1 f = new Fragment1();
+		Bundle b = new Bundle();
+		b.putString("this is a dialog with same simple text...", $title);
+		f.setArguments(b);
+		return f;
+	}
+	
+	public Dialog onCreateDialog(Bundle $b){
+		String s = $b.getString("title");
+		return new AlertDialog.Builder(getActivity())
+				.setIcon(R.drawable.ic_launcher)
+				.setTitle(s)
+				.setPositiveButton("OK", 
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int which) {
+								Toast.makeText(getActivity(), "OK" , Toast.LENGTH_SHORT).show();;
+							}
+				})
+				.setNegativeButton("Cancel", 
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int which) {
+								Toast.makeText(getActivity(), "Cancel", Toast.LENGTH_SHORT).show();
+							}
+				})
+				
+				.setMultiChoiceItems(items, itemsChecked, new DialogInterface.OnMultiChoiceClickListener() {
+					public void onClick(DialogInterface dialog , int which, boolean isChecked) {
+						String str = items[which] + (isChecked ? "checked" : "unchecked");
+						Toast.makeText(getBaseContext(), str, Toast.LENGTH_SHORT);
+					}
+				});
+	}
+	
+}
